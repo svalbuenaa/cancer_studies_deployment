fa8d6d502320a89aabf99dcb9944ee1f1d247d64
svalbuenaa <31705507+svalbuenaa@users.noreply.github.com>
2025-08-27 13:15:27 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/cancer_studies_deployment/',
+  base: '/cancer_studies_deployment/', 
   plugins: [react()],
 })
