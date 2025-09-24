02122e61ff336950d350a46f723159fae92fe020
svalbuenaa <31705507+svalbuenaa@users.noreply.github.com>
2025-09-24 10:33:48 +0200

    400 px height in small screens

--- src/components/MapCummulativeIncidence.jsx
+++ src/components/MapCummulativeIncidence.jsx
@@ -166,7 +166,7 @@ const MapCummulativeIncidence = ({ csvPath }) => {
         }}
         config={config}
         useResizeHandler={true}
-        style={{ width: "100%", height: windowWidth <= 768 ? 500 : 650 }}
+        style={{ width: "100%", height: windowWidth <= 768 ? 400 : 650 }}
       />
     </div>
   );
--- src/components/MapSelectedCancer.jsx
+++ src/components/MapSelectedCancer.jsx
@@ -175,7 +175,7 @@ const MapSelectedCancer = ({ csvPath }) => {
         }}
         config={config}
         useResizeHandler={true}
-        style={{ width: "100%", height: windowWidth <= 768 ? 500 : 650 }}
+        style={{ width: "100%", height: windowWidth <= 768 ? 400 : 650 }}
       />
       <div style={{ marginTop: "20px", textAlign: "center" }}>
         <label htmlFor="cancer-select" style={{ marginRight: "10px" }}>
